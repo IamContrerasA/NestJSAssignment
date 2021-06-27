6a06b2daaceee2ab555e1d28e386198a98b87f47
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-06-26 19:06:02 -0500

    trying to add some tests in users

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -4,11 +4,14 @@ import { Connection, Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { NotFoundException } from '@nestjs/common';
+import { UserRole } from './enum/user-enum';
 
 type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 const createMockRepository = <T = any>(): MockRepository<T> => ({
   findOne: jest.fn(),
   create: jest.fn(),
+  save: jest.fn(),
+  preload: jest.fn(),
 });
 
 describe('UsersService', () => {
@@ -59,6 +62,37 @@ describe('UsersService', () => {
           expect(err.message).toEqual(`User #${userId} not found`);
         }
       });
+    });
+  });
+
+  describe('create', () => {
+    it('should return the user object', async () => {
+      const userData = {
+        email: 'email@email.com',
+        password: 'password',
+        role: UserRole.CLIENT,
+      };
+      const expectedUser = undefined;
+
+      userRepository.create.mockReturnValue(expectedUser);
+      const user = await service.create(userData);
+      expect(user).toEqual(expectedUser);
+    });
+  });
+
+  describe('update', () => {
+    it('should return the user updated', async () => {
+      const id = '1';
+      const userData = {
+        email: 'email2@email.com',
+        password: 'password',
+        role: UserRole.CLIENT,
+      };
+      const expectedUser = {};
+
+      userRepository.preload.mockReturnValue(expectedUser);
+      const user = await service.update(id, userData);
+      expect(user).toMatchInlineSnapshot(`undefined`);
     });
   });
 });
